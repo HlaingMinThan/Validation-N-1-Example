04dff386d700977b70e1097eb35d4d1c395ea002
HlaingMinThan <hlaingminthan@92@gmail.com>
2023-10-10 11:29:30 +0630

    solution done

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,30 @@
 <?php
 
+use App\Models\Product;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rule;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/orders', function () {
     DB::enableQueryLog();
     try {
-        request()->validate([
+        validator(request()->all(), [
             'products' =>  ['required', 'array'],
-            'products.*.id' =>  ['required', Rule::exists('products', 'id')],
-        ]);
+            'products.*.id' =>  ['required']
+        ])
+            ->after(function (Validator $validator) {
+                //custom validation logic  
+                $requestProductIds = collect(request('products'))->pluck('id');
+                $realProductsIds = Product::whereIn('id', $requestProductIds)->pluck('id');
+                $requestProductIds->each(function ($id, $index) use ($realProductsIds, $validator) {
+                    if (!$realProductsIds->contains($id)) {
+                        //add error into the error bags
+                        $validator->errors()->add('products.' . $index . ".id", "Selected Products." . $index . ".id is invalid");
+                    }
+                });
+            })
+            ->validate();
         return DB::getQueryLog();
     } catch (Exception $e) {
         return $e->getMessage();
